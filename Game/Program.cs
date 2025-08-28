1496a26bd9e94ac2374d6832e9d9d5cde1c22fb5
kit <kit@DESKTOP-EHKDJE1>
2025-08-28 18:46:52 +0300

    GAME

---
+++ Game/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+           
+
+
+            int x = 30; // начальные координаты по горизонтали
+            int y = 10;  // начальные координаты по вертикали
+            ConsoleKeyInfo keyInfo;
+
+            Console.CursorVisible = false; // скрываем курсор
+
+            while (true)
+            {
+                Console.Clear();
+                Console.SetCursorPosition(x, y);
+
+                Console.Write("SPU-411"); // символ
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                keyInfo = Console.ReadKey(true); // ожидаем нажатие, не выводим символ
+                
+                
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.W:
+                        if (y > 0) y--;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+
+                    case ConsoleKey.S:
+                       // Console.Beep();
+                        if (y < Console.WindowHeight - 1) y++;
+                        break;
+                    case ConsoleKey.A:
+                        if (x > 0) x--;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case ConsoleKey.D:
+                        if (x < Console.WindowWidth - 1) x++;
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                       
+                    case ConsoleKey.Escape:
+                        return; // выход из программы по Esc
+                }
+            }
+            
+        }
+
+    }
+    
+}
